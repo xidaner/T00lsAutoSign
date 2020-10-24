5cab6669a0ea947ea7da3aeed68b61e1049fbdc8
iSec <isafe@users.noreply.github.com>
2020-10-24 15:52:50 +0800

    Update t00ls.py

--- t00ls.py
+++ t00ls.py
@@ -1,94 +1,90 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+import json
 import requests
-import re
-import time
-import hashlib
 import logging
+
 logging.basicConfig(level=logging.WARNING,
                     format='%(asctime)s - %(filename)s [line:%(lineno)d] - %(levelname)s: %(message)s')
-                    # logging.basicConfig函数对日志的输出格式及方式做相关配置
-# 由于日志基本配置中级别设置为DEBUG，所以一下打印信息将会全部显示在控制台上
+
 # logging.info('this is a loggging info message')
 # logging.debug('this is a loggging debug message')
 # logging.warning('this is loggging a warning message')
 # logging.error('this is an loggging error message')
 # logging.critical('this is a loggging critical message')
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
-}
+username = os.environ['USERNAME']       # 帐号
+password = os.environ['PASSWORD']       # 密码MD5 32位(小写)
+question_num = os.environ['QUESTION']   # 安全提问 参考下面
+question_answer = os.environ['ANSWER']  # 安全提问答案
 
-url_login = 'https://www.t00ls.net/login.html'
-url_checklogin = 'https://www.t00ls.net/checklogin.html'
-url_signin = 'https://www.t00ls.net/ajax-sign.json'
-
-# questionid
-# 1 母亲的名字
-# 2 爷爷的名字
-# 3 父亲出生的城市
-# 4 您其中一位老师的名字
-# 5 您个人计算机的型号
-# 6 您最喜欢的餐馆名称
-# 7 驾驶执照的最后四位数字
-
-username = os.environ['USERNAME']          # 用户名
-password = os.environ['PASSWORD']          # 明文密码或密码MD5
-password_hash = False                      # 密码为md5时设置为True
-questionid = os.environ['QUESTION']        # 问题ID，参考上面注释
-answer = os.environ['ANSWER']              # 问题答案
+# 0 = 没有安全提问
+# 1 = 母亲的名字
+# 2 = 爷爷的名字
+# 3 = 父亲出生的城市
+# 4 = 您其中一位老师的名字
+# 5 = 您个人计算机的型号
+# 6 = 您最喜欢的餐馆名称
+# 7 = 驾驶执照的最后四位数字
 
 
-def get_formhash(req):
-    res = req.get(url=url_login, headers=headers, timeout=15)
-    formhash_1 = re.findall('value="[0-9a-f]{8}"', res.content.decode("utf-8"))
-    formhash = re.findall('[0-9a-f]{8}', formhash_1[0])[0]
-    time.sleep(1)
-    return formhash
-
-def get_current_user(res):
-    current_user = re.findall('<a href="members-profile-[\d+].*\.html" target="_blank">{username}</a>'.format(username=username), res.decode("utf-8"))
-    cuser = re.findall('[\d+]{4,5}', ''.join(current_user))[0]
-    return cuser
-
-def login_t00ls(req):
-    formhash = get_formhash(req)
-    passwords = password if password_hash else hashlib.md5(password.encode("utf8")).hexdigest()
-    data = {
-        'username': username,
-        'password': passwords,
-        'questionid': questionid,
-        'answer': answer,
-        'formhash': formhash,
-        'loginsubmit': '登录',
-        'redirect': 'https://www.t00ls.net',
-        'cookietime': '2592000'
+def t00ls_login(u_name, u_pass, q_num, q_ans):
+    """
+    t00ls 登录函数
+    :param u_name: 用户名
+    :param u_pass: 密码的 md5 值 32 位小写
+    :param q_num: 安全提问类型
+    :param q_ans: 安全提问答案
+    :return: 签到要用的 hash 和 登录后的 Cookies
+    """
+    login_data = {
+        'action': 'login',
+        'username': u_name,
+        'password': u_pass,
+        'questionid': q_num,
+        'answer': q_ans
     }
-    headers['Referer'] = 'https://www.t00ls.net/'
-    res = req.post(url=url_login, headers=headers, data=data, timeout=15)
-    time.sleep(1)
-    return res, formhash
+    response_login = requests.post('https://www.t00ls.net/login.json', data=login_data)
+    response_login_json = json.loads(response_login.text)
 
-def get_formhash_1(req):
-    res = req.get(url=url_checklogin, headers=headers, timeout=15)
-    uid = get_current_user(res.content)
-    formhash = re.findall('[0-9a-f]{8}', res.content.decode("utf-8"))[0]
-    return formhash, uid
+    if response_login_json['status'] != 'success':
+        return None
+    else:
+        logging.warning("用户: {0} 登入成功!".format(username))
+        formhash = response_login_json['formhash']
+        t00ls_cookies = response_login.cookies
+        return formhash, t00ls_cookies
 
-def signin_t00ls(req):
-    formhash, uid = get_formhash_1(req)
-    data = {
-        'formhash': formhash,
-        'signsubmit': 'apply'
+
+def t00ls_sign(t00ls_hash, t00ls_cookies):
+    """
+    t00ls 签到函数
+    :param t00ls_hash: 签到要用的 hash
+    :param t00ls_cookies: 登录后的 Cookies
+    :return: 签到后的 JSON 数据
+    """
+    sign_data = {
+        'formhash': t00ls_hash,
+        'signsubmit': "true"
     }
-    headers['Referer'] = 'https://www.t00ls.net/members-profile-{uid}.html'.format(uid=uid)
-    res = req.post(url=url_signin, data=data, headers=headers, timeout=15)
-    return res
+    response_sign = requests.post('https://www.t00ls.net/ajax-sign.json', data=sign_data, cookies=t00ls_cookies)
+    return json.loads(response_sign.text)
 
-if '__main__' == __name__:
-    req = requests.session()
-    res_login, formhash = login_t00ls(req)
-    res_signin = signin_t00ls(req)
-    logging.warning(res_signin.json())
+
+def main():
+    response_login = t00ls_login(username, password, question_num, question_answer)
+    if response_login:
+        response_sign = t00ls_sign(response_login[0], response_login[1])
+        if response_sign['status'] == 'success':
+            logging.warning("签到成功")
+        elif response_sign['message'] == 'alreadysign':
+            logging.warning("今日已签到")
+        else:
+            logging.error("出现玄学问题了 签到失败")
+    else:
+        logging.error("登录失败,请检查输入资料是否正确")
+
+
+if __name__ == '__main__':
+    main()
